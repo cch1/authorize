de54fbd0454768caa6249682f6be5d8ec24612d8
Chris Hapgood <cch1@hapgoods.com>
2008-08-06 18:06:56 -0400

    Added controller tests.

--- test/controller_test.rb
+++ test/controller_test.rb
@@ -6,6 +6,19 @@ class ControllerTest < ActionController::TestCase
   
   tests WidgetsController
   
-  test 'should do something' do
+  test 'should indicate authorization with predicate' do
+    @controller.instance_variable_set(:@w, widgets(:foo))
+    assert @controller.permit?('owner of w', {:user => users(:chris)})
+    assert !@controller.permit?('proxy of w', {:user => users(:chris)})
+  end
+
+  test 'should indicate authorization with block processing' do
+    @controller.instance_variable_set(:@w, widgets(:foo))
+    assert_nothing_raised do
+      @controller.permit('owner of w', {:user => users(:chris)}) {}
+    end
+    assert_raises Authorize::AuthorizationError do
+      @controller.permit('proxy of w', {:user => users(:chris)}) {}
+    end
   end
 end
--- test/lib/user.rb
+++ test/lib/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   cattr_accessor :current # Class attribute automatically set after authentication
   
   acts_as_trustee
+  
+  def identities
+    [self.id]
+  end
 end
