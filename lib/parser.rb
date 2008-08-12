dea91dbc438f52d7507f521b292a6df0d9318202
Chris Hapgood <cch1@hapgoods.com>
2008-08-12 13:08:56 -0400

    Refactored parser.
     * use a single Regexp to identify and gsub all terms.
     * replace all terms with boolean equivalent on first pass.

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,9 +1,8 @@
 module Authorize
   module Base
-    
     PREPOSITIONS = %w(of for in on to at by)
     BOOLEANS = %w(not or and true false)
-        
+    
     module EvalParser
       # Parses and evaluates an authorization expression and returns <tt>true</tt> or <tt>false</tt>.
       #
@@ -11,56 +10,38 @@ module Authorize
       #         <expr> ::= (<expr>) | not <expr> | <term> or <expr> | <term> and <expr> | <term>
       #         <term> ::= <role> | <role> <preposition> <model> | <role> <preposition> <class>
       #  <preposition> ::= of | for | in | on | to | at | by
-      #        <model> ::= /:*\w+/
-      #         <role> ::= /\w+/ | /'.*'/
+      #        <model> ::= /\w+/
+      #         <role> ::= /\w+/
       #
-      # Instead of doing recursive descent parsing (not so fun when we support nested parentheses, etc),
-      # we let Ruby do the heavy lifting by identifying terms and replacing them with their boolean equivalents
-      # and then letting Ruby eval the resulting expression.
-      #
-      # 1) Replace all <role> <preposition> <model> matches with their boolean truth state.
-      # 2) Replace all <role> matches (carefully avoiding grammer elements) with their boolean truth state.
-      # 3) Eval the resulting string
-
-      def parse_authorization_expression( str )
-        if str =~ /[^A-Za-z0-9_:'\(\)\s]/
+      # We let Ruby do the heavy lifting by identifying terms and replacing them with their boolean equivalents
+      # and then letting Ruby eval the resulting boolean expression.
+      def parse_authorization_expression(str)
+        if str =~ /[^A-Za-z0-9_\(\)\s]/
           raise AuthorizationExpressionInvalid, "Invalid authorization expression (#{str})"
           return false
         end
         begin
-          expr = replace_role_of_model(str)
-          expr = replace_role(expr)
+          expr = replace_terms(str)
           instance_eval(expr)
         rescue CannotObtainModelObject, CannotObtainUserObject => e
           raise e
-          false
         rescue => e
-          raise AuthorizationExpressionInvalid, "Cannot parse authorization expression (#{str}):#{e.to_s}"
-          false
+          raise AuthorizationExpressionInvalid, "Cannot parse authorization expression:#{e.to_s}"
         end
       end
 
-      def replace_role_of_model(str)
-        role_regex = '\s*(\'\s*(.+)\s*\'|(\w+))\s+'
-        model_regex = '\s+(:*\w+)'
-        parse_regex = Regexp.new(role_regex + '(' + PREPOSITIONS.join('|') + ')' + model_regex)
-        str.gsub(parse_regex) do |match|
-          " #{process_term($2 || $3, $5)} "
+      # This method replaces term strings with their processed (boolean) equivalent.  Term strings are either
+      # <role> <preposition> <model-or-class> or simply <role>.  The regular expression used to find term strings
+      # looks for either pairs of words separated by prepositions or, using negative lookahead, words that are not
+      # part of the Ruby boolean language.
+      def replace_terms(string)
+        re = /(\w+)\s(?:#{PREPOSITIONS.join('|')})\s(\w+)|\b(?!#{BOOLEANS.join('|')})(\w+)\b/
+        string.gsub(re) do |match|
+          params = [$1, $2, $3].compact
+          process_term(*params).to_s
         end
       end
 
-      def replace_role(str)
-        role_regex = '\s*(\'\s*(.+)\s*\'|([A-Za-z]\w*))\s*'
-        parse_regex = Regexp.new(role_regex)
-        str.gsub(parse_regex) do |match|
-          if BOOLEANS.include?($3)
-            " #{match} "
-          else
-          " #{process_term($2 || $3)} "
-          end
-        end
-      end
-      
       # Determine if any of the authorized_identities are authorized as the given role over the given subject.
       # We cache the authorized roles to optimize the common pattern of "role1 of widget or role2 of widget or role3 of widget".
       def process_term(role, model_name = nil)
