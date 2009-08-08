a64e3fc7221276efd7e89233ad21ba561bff61b3
Chris Hapgood <cch1@hapgoods.com>
2009-08-07 23:27:16 -0400

    Remove User#authorization_tokens as it is nearly worthless

--- test/application/app/models/user.rb
+++ test/application/app/models/user.rb
@@ -2,8 +2,4 @@ class User < ActiveRecord::Base
   cattr_accessor :current # Class attribute automatically set after authentication
   
   acts_as_trustee
-  
-  def authorization_tokens
-    [self.authorization_token]
-  end
 end
