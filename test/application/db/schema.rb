489e568e48487a9c3c5dfc7afed88744c6290b16
Chris Hapgood <cch1@hapgoods.com>
2009-08-26 16:07:41 -0400

    Update test schema for compatibility with MySQL
    * Override default index name to comply with MySQL length limitations.
    * Change field type for tokens so that they can be used in an index.

--- test/application/db/schema.rb
+++ test/application/db/schema.rb
@@ -1,7 +1,7 @@
 ActiveRecord::Schema.define(:version => 0) do
   create_table :users, :force => true do |t|
     t.string :login, :limit => 80
-    t.text :authorization_token, :limit => Authorize::Token::SIZE
+    t.string :authorization_token, :limit => Authorize::Token::SIZE
     t.timestamps
   end
   create_table :degenerate_users, :force => true do |t|
@@ -13,12 +13,12 @@ ActiveRecord::Schema.define(:version => 0) do
   end
   create_table :authorizations, :force => true do |t|
       t.string :role, :limit => 20
-      t.text :token
+      t.string :token
       t.integer :subject_id
       t.string :subject_type, :limit => 25
       t.integer :parent_id
       t.timestamps
   end
-  add_index :authorizations, [:role, :token, :subject_id, :subject_type], :unique
-  add_index :authorizations, [:token, :subject_id, :subject_type, :role], :unique
+  add_index :authorizations, [:role, :token, :subject_id, :subject_type], :unique => true, :name => "rts"
+  add_index :authorizations, [:token, :subject_id, :subject_type, :role], :unique => true, :name => "tsr"
 end
