d26c4c77e9bc9b8c1a8f6a2c9529dc6062acfa83
Chris Hapgood <cch1@hapgoods.com>
2010-04-04 21:42:38 -0400

    Pre-build Regexps for performance

--- lib/authorize/identity.rb
+++ lib/authorize/identity.rb
@@ -1,49 +1,55 @@
-# Provides the appearance of dynamically generated methods on the roles database.  Note that none of these methods 
+# Provides the appearance of dynamically generated methods on the roles database.
 #
 # Examples:
-#   trustee.is_member?                     --> Returns true if user has any authorization with role "member"
-#   trustee.is_member_of? this_workshop    --> Returns true/false. Must have subject object after query.
-#   trustee.is_proxy_for this_client       --> Gives user authorization as "proxy" for "client"
-#   trustee.is_moderator                   --> Gives user "moderator" authorization (not tied to any class or object)
-#   trustee.is_candidate_of_what           --> Returns array of subjects for which this user has authorization as a "candidate"
+#   trustee.is_role?                       --> Predicate.  Returns boolean based on trustee having global role
+#   trustee.is_role_of? subject            --> Predicate.  Returns boolean based on trustee having role of subject
+#   trustee.is_role_for subject            --> Imperative.  Gives user authorization as role for subject
+#   trustee.is_role                        --> Imperative.  Gives user global role authorization
+#   trustee.is_role_of_what                --> Returns array of subjects for which trustee is role
 #
-#   subject.has_members                   --> Returns array of trustees as "member" authorized on subject
-#   subject.has_members?                  --> Returns true/false
+#   subject.has_roles                      --> Returns array of trustees having role over the subject
+#   subject.has_roles?                     --> Predicate.  Returns boolean based having at least one trustee with role
 module Authorize
   module Identity
-    
+
     module TrusteeExtensions
       module InstanceMethods
+        base = "is_(\\w+)"
+        fancy = base + "_(#{Authorize::Expression::PREPOSITIONS.join('|')})"
+        is_either = '^((' + fancy + ')|(' + base + '))'
+        base_not = "is_no[t]?_(\\w+)"
+        fancy_not = base_not + "_(#{Authorize::Expression::PREPOSITIONS.join('|')})"
+        is_not_either = '^((' + fancy_not + ')|(' + base_not + '))'
         # TODO: Use a single sophisticated Regexp, like this: /^(is)(_)(not)?(\w+?)(_)(#{Authorize::Expression::PREPOSITIONS.join('|')})?(\?)?$/
+        EITHER_SUBJECTS = /#{is_either}_what$/
+        NEITHER_PREDICATE = /#{is_not_either}\?$/
+        EITHER_PREDICATE = /#{is_either}\?$/
+        NOT_EITHER = /#{is_not_either}$/
+        EITHER = /#{is_either}$/
+
         def method_missing(method, *args)
           subject = args.first
-          base_regex = "is_(\\w+)"
-          fancy_regex = base_regex + "_(#{Authorize::Expression::PREPOSITIONS.join('|')})"
-          is_either_regex = '^((' + fancy_regex + ')|(' + base_regex + '))'
-          base_not_regex = "is_no[t]?_(\\w+)"
-          fancy_not_regex = base_not_regex + "_(#{Authorize::Expression::PREPOSITIONS.join('|')})"      
-          is_not_either_regex = '^((' + fancy_not_regex + ')|(' + base_not_regex + '))'
           case method.to_s
-            when Regexp.new(is_either_regex + '_what$')
+            when EITHER_SUBJECTS
               role = $3 || $6
               subjects_as(role)
-            when Regexp.new(is_not_either_regex + '\?$')
+            when NEITHER_PREDICATE
               role = $3 || $6
               not authorized?(role, subject)
-            when Regexp.new(is_either_regex + '\?$')
+            when EITHER_PREDICATE
               role = $3 || $6
               authorized?(role, subject)
-            when Regexp.new(is_not_either_regex + '$')
+            when NOT_EITHER
               role = $3 || $6
               unauthorize(role, subject)
-            when Regexp.new(is_either_regex + '$')
+            when EITHER
               role = $3 || $6
               authorize(role, subject)
             else
               super
           end
         end
-      
+
         private
         def subjects_as(role)
           authorizations.as(role).map do |a|
@@ -58,15 +64,18 @@ module Authorize
         end
       end # InstanceMethods Module
     end # TrusteeExtensions Module
-    
+
     module SubjectExtensions
       module InstanceMethods
+        TRUSTEES_PREDICATE = /^has_(\w+)\?$/
+        TRUSTEES = /^has_(\w+)$/
+
         def method_missing(method_sym, *args)
           method_name = method_sym.to_s
-          if method_name =~ /^has_(\w+)\?$/
+          if method_name =~ TRUSTEES_PREDICATE
             role = $1.singularize
             self.subjections.as(role).any?
-          elsif method_name =~ /^has_(\w+)$/
+          elsif method_name =~ TRUSTEES
             role = $1.singularize
             self.subjections.as(role).map { |auth| auth.trustee }
           else
