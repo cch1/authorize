f689d4c6dff64756828aa9d5e3d738696966c551
Chris Hapgood <cch1@hapgoods.com>
2010-04-04 22:40:14 -0400

    Raise exception when indicated object is missing
    When a preposition is present but its object is missing, raise an exception.  This
    prevents innocent-looking constructs like "is_owner_of(foo)" from ever generating a
    global authorization.

--- lib/authorize/identity.rb
+++ lib/authorize/identity.rb
@@ -1,50 +1,51 @@
 # Provides the appearance of dynamically generated methods on the roles database.
 #
 # Examples:
-#   trustee.is_role?                       --> Predicate.  Returns boolean based on trustee having global role
-#   trustee.is_role_of? subject            --> Predicate.  Returns boolean based on trustee having role of subject
-#   trustee.is_role_for subject            --> Imperative.  Gives user authorization as role for subject
-#   trustee.is_role                        --> Imperative.  Gives user global role authorization
-#   trustee.is_role_of_what                --> Returns array of subjects for which trustee is role
+#   trustee.is_role?                      --> Predicate.  Returns boolean based on trustee having global role
+#   trustee.is_role_of? object            --> Predicate.  Returns boolean based on trustee having role of object
+#   trustee.is_role_for object            --> Imperative.  Gives user authorization as role for object
+#   trustee.is_role                       --> Imperative.  Gives user global role authorization
+#   trustee.is_role_of_what               --> Returns array of objects for which trustee is role
 #
-#   subject.has_roles                      --> Returns array of trustees having role over the subject
-#   subject.has_roles?                     --> Predicate.  Returns boolean based having at least one trustee with role
+#   object.has_roles                      --> Returns array of trustees having role over the object
+#   object.has_roles?                     --> Predicate.  Returns boolean based having at least one trustee with role
 module Authorize
   module Identity
 
     module TrusteeExtensions
       module InstanceMethods
+        REJECT_WHEN_MISSING_OBJECT_OF_PREPOSITION = true
+
         base = "is_(\\w+)"
-        fancy = base + "_(#{Authorize::Expression::PREPOSITIONS.join('|')})"
-        is_either = '^((' + fancy + ')|(' + base + '))'
         base_not = "is_no[t]?_(\\w+)"
-        fancy_not = base_not + "_(#{Authorize::Expression::PREPOSITIONS.join('|')})"
-        is_not_either = '^((' + fancy_not + ')|(' + base_not + '))'
-        # TODO: Use a single sophisticated Regexp, like this: /^(is)(_)(not)?(\w+?)(_)(#{Authorize::Expression::PREPOSITIONS.join('|')})?(\?)?$/
-        EITHER_SUBJECTS = /#{is_either}_what$/
-        NEITHER_PREDICATE = /#{is_not_either}\?$/
-        EITHER_PREDICATE = /#{is_either}\?$/
-        NOT_EITHER = /#{is_not_either}$/
-        EITHER = /#{is_either}$/
+        IMPERATIVE = /^#{base}$/
+        IMPERATIVE_WITH_OBJECT = /^#{base}_(#{Authorize::Expression::PREPOSITIONS.join('|')})$/
+        INVERSE_IMPERATIVE = /^#{base_not}$/
+        INVERSE_IMPERATIVE_WITH_OBJECT = /^#{base_not}_(#{Authorize::Expression::PREPOSITIONS.join('|')})$/
+        PREDICATE = /^#{base}\?$/
+        PREDICATE_WITH_OBJECT = /^#{base}_(#{Authorize::Expression::PREPOSITIONS.join('|')})\?$/
+        OBJECTS = /^#{base}_(#{Authorize::Expression::PREPOSITIONS.join('|')})_what$/
 
         def method_missing(method, *args)
-          subject = args.first
+          object = args.first
           case method.to_s
-            when EITHER_SUBJECTS
-              role = $3 || $6
-              subjects_as(role)
-            when NEITHER_PREDICATE
-              role = $3 || $6
-              not authorized?(role, subject)
-            when EITHER_PREDICATE
-              role = $3 || $6
-              authorized?(role, subject)
-            when NOT_EITHER
-              role = $3 || $6
-              unauthorize(role, subject)
-            when EITHER
-              role = $3 || $6
-              authorize(role, subject)
+            when OBJECTS
+              subjects_as($1)
+            when PREDICATE_WITH_OBJECT
+              raise AuthorizationExpressionInvalid if object.nil? && REJECT_WHEN_MISSING_OBJECT_OF_PREPOSITION
+              authorized?($1, object)
+            when PREDICATE
+              authorized?($1)
+            when INVERSE_IMPERATIVE_WITH_OBJECT
+              raise AuthorizationExpressionInvalid if object.nil? && REJECT_WHEN_MISSING_OBJECT_OF_PREPOSITION
+              unauthorize($1, object)
+            when INVERSE_IMPERATIVE
+              unauthorize($1)
+            when IMPERATIVE_WITH_OBJECT
+              raise AuthorizationExpressionInvalid if object.nil? && REJECT_WHEN_MISSING_OBJECT_OF_PREPOSITION
+              authorize($1, object)
+            when IMPERATIVE
+              authorize($1)
             else
               super
           end
--- test/identity_test.rb
+++ test/identity_test.rb
@@ -27,6 +27,28 @@ class IdentityTest < ActiveSupport::TestCase
     assert_equal 1, Authorization.as('advocate').count
   end
 
+  test 'should implement is_not_role dynamic destructor' do
+    assert users(:chris).is_not_overlord
+    assert_equal 0, Authorization.as('overlord').with(users(:chris).authorization_token).count
+  end
+
+  test 'should implement is_not_role_of_subject dynamic destructor' do
+    assert users(:pascale).is_not_proxy_of widgets(:bar)
+    assert_equal 0, Authorization.as('proxy').with(users(:pascale).authorization_token).over(widgets(:bar)).count
+  end
+
+  test 'should implement is_role_of subject dynamic creator' do
+    assert users(:chris).is_advocate_of(widgets(:foo))
+    assert_equal 1, Authorization.as('advocate').count
+  end
+
+  test 'should implement is_role_of subject dynamic creator with subject check' do
+    assert_raises Authorize::AuthorizationExpressionInvalid do
+      users(:chris).is_advocate_of
+    end
+    assert_equal 0, Authorization.as('advocate').count
+  end
+
   test 'should implement is_role_of_what dynamic finders' do
     assert minions = users(:alex).is_overlord_of_what
     assert_equal 1, minions.size
