14fbfe18b30d32a13ce06be1cc59a3d7d52a00da
Chris Hapgood <cch1@hapgoods.com>
2010-09-03 16:09:37 -0400

    Fix bug in test helper

--- lib/authorize/test_helper.rb
+++ lib/authorize/test_helper.rb
@@ -20,12 +20,12 @@ module Authorize
 
     def assert_has_role(tor, subrole)
       role = tor.is_a?(Authorize::Role) ? tor : tor.role
-      assert_block("Role #{role} does not include #{subrole}") {role.roles.include?(subrole))}
+      assert_block("Role #{role} does not include #{subrole}") {role.roles.include?(subrole)}
     end
 
     def assert_does_not_have_role(tor, subrole)
       role = tor.is_a?(Authorize::Role) ? tor : tor.role
-      assert_block("Role #{role} includes #{subrole}") {!role.roles.include?(subrole))}
+      assert_block("Role #{role} includes #{subrole}") {!role.roles.include?(subrole)}
     end
   end
 end
