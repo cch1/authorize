2e5787bdcfaacc12dfaa01b00abc8a50a964e19f
Chris Hapgood <cch1@hapgoods.com>
2010-09-03 18:55:28 -0400

    Clean up trustee/resource associations

--- lib/authorize/active_record.rb
+++ lib/authorize/active_record.rb
@@ -3,18 +3,21 @@ module Authorize
     def self.included(recipient)
       recipient.extend(ClassMethods)
     end
-    
+
     module ClassMethods
       def authorizable_trustee
         include Authorize::Trustee
-        has_one :role, :class_name => "Authorize::Role", :as => :resource, :conditions => {:name => nil}, :dependent => :destroy
-        has_many :roles
+        # The "identity" role -the single role that represents this trustee.  It is also the root vertex for collecting
+        # the set of roles belonging to the trustee.
+        has_one :role, :class_name => "Authorize::Role", :as => :resource, :conditions => {:relation => nil}, :dependent => :destroy
         after_create :create_role
       end
-  
+
       def authorizable_resource
         include Authorize::Resource
         has_many :permissions, :class_name => "Authorize::Permission", :as => :resource, :dependent => :delete_all
+        # The roles that represent relations/associations of a trustee
+        has_many :roles, :class_name => "Authorize::Role", :as => :resource
         reflection = reflections[:permissions]
         auth_fk = "#{reflection.quoted_table_name}.#{connection.quote_column_name(reflection.primary_key_name)}"
         resource_pk = "#{connection.quote_table_name(table_name)}.#{connection.quote_column_name(primary_key)}"
