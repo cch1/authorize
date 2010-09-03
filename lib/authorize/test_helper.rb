da7e36cfce87433d54b9d934cc0c4956b009340b
Chris Hapgood <cch1@hapgoods.com>
2010-09-03 16:03:13 -0400

    Expand test helpers with role inclusion assertions

--- lib/authorize/test_helper.rb
+++ lib/authorize/test_helper.rb
@@ -17,5 +17,15 @@ module Authorize
       role = tor.is_a?(Authorize::Role) ? tor : tor.role
       assert_block("Role #{role} is authorized") {role.may_not?(*args)}
     end
+
+    def assert_has_role(tor, subrole)
+      role = tor.is_a?(Authorize::Role) ? tor : tor.role
+      assert_block("Role #{role} does not include #{subrole}") {role.roles.include?(subrole))}
+    end
+
+    def assert_does_not_have_role(tor, subrole)
+      role = tor.is_a?(Authorize::Role) ? tor : tor.role
+      assert_block("Role #{role} includes #{subrole}") {!role.roles.include?(subrole))}
+    end
   end
 end
