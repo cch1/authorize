1f1aa05e7de1f9e382960b980402ea6cd2406f55
Chris Hapgood <cch1@hapgoods.com>
2010-09-16 23:12:36 -0400

    Add assert_permitted helper

--- lib/authorize/test_helper.rb
+++ lib/authorize/test_helper.rb
@@ -3,7 +3,7 @@ require 'authorize/graph/fixtures'
 
 module Authorize
   module TestHelper
-    # Assert that a given role explicitly has a given permission mode over a given resource
+    # Assert that a given role explicitly has a given permission mode for a given resource
     # Example: assert_authorized(current_user, :read, :list, widget)
     # If a trustee is provided instead of a role, then the primary role of the trustee is used.
     def assert_authorized(*args)
@@ -26,6 +26,17 @@ module Authorize
     def assert_does_not_have_role(tor, subrole)
       role = tor.is_a?(Authorize::Role) ? tor : tor.role
       assert_block("Role #{role} includes #{subrole}") {!role.roles.include?(subrole)}
+    end
+
+    # A weak assertion of access
+    # The assertion includes subordinate roles' permissions and considers global/class permissions
+    def assert_permitted(tor, *args)
+      role = tor.is_a?(Authorize::Role) ? tor : tor.role
+      resource = args.pop
+      request_mask = Authorize::Permission::Mask[*args]
+      assert_block("Role #{role} is not permitted (#{request_mask})") do
+        Authorize::Permission.over(resource).as(role.roles).permit?(request_mask)
+      end
     end
   end
 end
