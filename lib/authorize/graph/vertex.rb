2746b676a055f0fb224807e5e9663870c25a3f05
Chris Hapgood <cch1@hapgoods.com>
2010-12-14 16:20:59 -0500

    Use more conventional method names

--- lib/authorize/graph/vertex.rb
+++ lib/authorize/graph/vertex.rb
@@ -3,26 +3,27 @@ module Authorize
     def self.exists?(id)
       super(subordinate_key(id, '_'))
     end
-  
+
     def initialize(properties = {})
       super()
       # Because a degenerate vertex can have neither properties nor edges, we must store a marker to indicate existence
       self.class.db.set(subordinate_key('_'), nil)
       merge(properties) if properties.any?
     end
-  
+
     def edge_ids
       Redis::Set.load(subordinate_key('edge_ids'))
     end
-  
+
     def edges
       edge_ids.map{|id| Graph::Edge.load(id)}.to_set
     end
-  
-    def neighbors
+
+    def adjancies
       edges.map{|e| e.right}
     end
-  
+    alias neighbors adjancies
+
     def traverse(options = {})
       Graph::Traverser.new(self)
     end
