d6655855fb1a74b1a7c805210cc59ac1b226ad1f
Chris Hapgood <cch1@hapgoods.com>
2011-05-24 12:39:58 -0400

    Use Bundler for test app as well
    Now we no longer depend on an installed authorize gem -we can use the
    version under development.

--- test/config/environment.rb
+++ test/config/environment.rb
@@ -18,9 +18,6 @@ Rails::Initializer.run do |config|
     :secret      => '7e54ff5913c5c26e6389fad599134e255845d537650386fb04e5ed9c34aaeea4538a3c50833e1f243210c54d612a388afb11a6c876af18d9ac31f1be4fc78698'
   }
 
-  config.gem 'redis', :version => '>=2.0'
-  config.gem 'authorize'
-
   config.after_initialize do
     ActiveRecord::Migration.verbose = false
     require File.join(RAILS_ROOT, "db", 'schema.rb')
